4adbc896f330f57a1d995cd174cd6ec3a5f6d541
Vladislav <vladv161095@gmail.com>
2019-04-18 10:21:16 +0300

    Posts template + Pagination

--- blogtheme/functions.php
+++ blogtheme/functions.php
@@ -46,3 +46,153 @@ function blogtheme_scripts() {
 	wp_enqueue_script( 'jquery.easing.min', get_template_directory_uri() . '/js/jquery.easing.min.js' );
 }
 add_action( 'wp_enqueue_scripts', 'blogtheme_scripts' );
+add_filter('excerpt_more', function($more){
+	return '';
+});
+
+function blogtheme_the_breadcrumb(){
+	global $post;
+	if(!is_home()){
+	   echo '<li><a href="'.site_url().'"><i class="fa fa-home" aria-hidden="true"></i>Home</a></li> <li> / </li> ';
+		if(is_single()){ // posts
+		the_category(', ');
+		echo " <li> / </li> ";
+		echo '<li>';
+			the_title();
+		echo '</li>';
+		}
+		elseif (is_page()) { // pages
+			if ($post->post_parent ) {
+				$parent_id  = $post->post_parent;
+				$breadcrumbs = array();
+				while ($parent_id) {
+					$page = get_page($parent_id);
+					$breadcrumbs[] = '<li><a href="' . get_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a></li>';
+					$parent_id  = $page->post_parent;
+				}
+				$breadcrumbs = array_reverse($breadcrumbs);
+				foreach ($breadcrumbs as $crumb) echo $crumb . '<li> / </li> ';
+			}
+			echo the_title();
+		}
+		elseif (is_category()) { // category
+			global $wp_query;
+			$obj_cat = $wp_query->get_queried_object();
+			$current_cat = $obj_cat->term_id;
+			$current_cat = get_category($current_cat);
+			$parent_cat = get_category($current_cat->parent);
+			if ($current_cat->parent != 0)
+				echo(get_category_parents($parent_cat, TRUE, ' <li> / </li> '));
+			single_cat_title();
+		}
+		elseif (is_search()) { // search pages
+			echo 'Search results "' . get_search_query() . '"';
+		}
+		elseif (is_tag()) { // tags
+			echo single_tag_title('', false);
+		}
+		elseif (is_day()) { // archive (days)
+			echo '<li><a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a></li> <li> / </li> ';
+			echo '<li><a href="' . get_month_link(get_the_time('Y'),get_the_time('m')) . '">' . get_the_time('F') . '</a></li> <li> / </li> ';
+			echo get_the_time('d');
+		}
+		elseif (is_month()) { // archive (months)
+			echo '<li><a href="' . get_year_link(get_the_time('Y')) . '">' . get_the_time('Y') . '</a></li> <li> / </li>';
+			echo get_the_time('F');
+		}
+		elseif (is_year()) { // archive (years)
+			echo get_the_time('Y');
+		}
+		elseif (is_author()) { // authors
+			global $author;
+			$userdata = get_userdata($author);
+			echo '<li>Posted ' . $userdata->display_name . '</li>';
+		} elseif (is_404()) { // if page not found
+			echo '<li>Error 404</li>';
+		}
+
+		if (get_query_var('paged')) // number of page
+			echo ' (' . get_query_var('paged').'- page)';
+
+	} else { // home
+	   $pageNum=(get_query_var('paged')) ? get_query_var('paged') : 1;
+	   if($pageNum>1)
+	      echo '<li><a href="'.site_url().'"><i class="fa fa-home" aria-hidden="true"></i>Home</a></li> <li> / </li> <li>'.$pageNum.'- page</li>';
+	   else
+	      echo '<li><i class="fa fa-home" aria-hidden="true"></i>Home</li>';
+	}
+}
+
+/**
+* Pagination
+*/
+function blogtheme_pagination( $args = array() ) {
+
+    $defaults = array(
+        'range'           => 4,
+        'custom_query'    => FALSE,
+        'previous_string' => __( 'Previous', 'text-domain' ),
+        'next_string'     => __( 'Next', 'text-domain' ),
+        'before_output'   => '<div class="next_page"><ul class="page-numbers">',
+        'after_output'    => '</ul></div>'
+    );
+
+    $args = wp_parse_args(
+        $args,
+        apply_filters( 'blogtheme_pagination_defaults', $defaults )
+    );
+
+    $args['range'] = (int) $args['range'] - 1;
+    if ( !$args['custom_query'] )
+        $args['custom_query'] = @$GLOBALS['wp_query'];
+    $count = (int) $args['custom_query']->max_num_pages;
+    $page  = intval( get_query_var( 'paged' ) );
+    $ceil  = ceil( $args['range'] / 2 );
+
+    if ( $count <= 1 )
+        return FALSE;
+
+    if ( !$page )
+        $page = 1;
+
+    if ( $count > $args['range'] ) {
+        if ( $page <= $args['range'] ) {
+            $min = 1;
+            $max = $args['range'] + 1;
+        } elseif ( $page >= ($count - $ceil) ) {
+            $min = $count - $args['range'];
+            $max = $count;
+        } elseif ( $page >= $args['range'] && $page < ($count - $ceil) ) {
+            $min = $page - $ceil;
+            $max = $page + $ceil;
+        }
+    } else {
+        $min = 1;
+        $max = $count;
+    }
+
+    $echo = '';
+    $previous = intval($page) - 1;
+    $previous = esc_attr( get_pagenum_link($previous) );
+
+    if ( $previous && (1 != $page) )
+        $echo .= '<li><a href="' . $previous . '"class="page-numbers" title="' . __( 'previous', 'text-domain') . '">' . $args['previous_string'] . '</a></li>';
+
+    if ( !empty($min) && !empty($max) ) {
+        for( $i = $min; $i <= $max; $i++ ) {
+            if ($page == $i) {
+                $echo .= '<li class="active"><span class="page-numbers current">' . str_pad( (int)$i, 1, '0', STR_PAD_LEFT ) . '</span></li>';
+            } else {
+                $echo .= sprintf( '<li><a href="%s" class="page-numbers">%2d</a></li>', esc_attr( get_pagenum_link($i) ), $i );
+            }
+        }
+    }
+
+    $next = intval($page) + 1;
+    $next = esc_attr( get_pagenum_link($next) );
+    if ($next && ($count != $page) )
+        $echo .= '<li><a href="' . $next . '" class="page-numbers" title="' . __( 'next', 'text-domain') . '">' . $args['next_string'] . '</a></li>';
+
+    if ( isset($echo) )
+        echo $args['before_output'] . $echo . $args['after_output'];
+}
--- blogtheme/index.php
+++ blogtheme/index.php
@@ -1,75 +1,42 @@
 <?php get_header(); ?>
-<section class="error_top_bg secondary-bg">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="top_banner">
-						<div class="category_m">
-							<h2> App News</h2>
-
-							<div class="breadcrumb_m">
-									  <ul class="breadcrumbs_m" id="breadcrumbs_m">
-									  <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i> Home </a></li>
-									  <li> / </li>
-									  <li><a href="#">  Blog</a></li>
-									  </ul>
-							</div>
-						</div>
-
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-
-
-
+<?php get_template_part('template-parts/breadcrumps'); ?>
 <section class="post_blog_bg primary-bg">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
 
         	<div class="col-md-8">
-				<article class="blog_post">
-					<h4> <a href="#"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. </a> </h4>
+						<?php if(have_posts()): ?>
+							<?php while(have_posts()) : the_post(); ?>
+								<article class="blog_post">
+									<h4> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h4>
 
-						<div class="blog_category">
-							<ul>
-								<li> <a href="#">Company news, </a> </li>
-								<li> <a href="#">Fresh releases </a> </li>
-							</ul>
-						</div>
+										<div class="blog_category">
+											<ul>
+												<li><?php the_category(', '); ?></li>
+											</ul>
+										</div>
 
-						<div class="blog_text">
-							<ul>
-								<li> | </li>
-								<li> <a href="#"> Post By : Admin   </a> </li>
-								<li> | </li>
-								<li>  On : 20 may 2016 </li>
-							</ul>
-						</div>
+										<div class="blog_text">
+											<ul>
+												<li> | </li>
+												<li> Post By : <?php the_author_posts_link(); ?>   </a> </li>
+												<li> | </li>
+												<li>  On : <?php the_time('j F Y');?> </li>
+											</ul>
+										</div>
 
-						<div class="blog_post_img">
-							<a href="#"> <img src="images/blog_post_img1.png" alt="image"> </a>
-						</div>
-
-						<p> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. </p>
-
-						<a href="#"> Continue reading <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+										<div class="blog_post_img">
+											<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?> </a>
+										</div>
+										<?php the_excerpt(); ?>
+										<a href="<?php the_permalink(); ?>"> Continue reading <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
 
 
-				</article>
-
-				<div class="next_page">
-          			<ul class="page-numbers">
-						<li><span class="page-numbers current">1</span></li>
-						<li><a href="#" class="page-numbers">2</a></li>
-						<li><a href="#" class="page-numbers">3</a></li>
-						<li><a href="#" class="page-numbers">4</a></li>
-						<li><a href="#" class="next page-numbers">Next</a></li>
-					</ul>
-       			 </div>
-
+								</article>
+							<?php endwhile; ?>
+						<?php endif; ?>
+						<?php blogtheme_pagination(); ?>
 			</div>
 						<!--sidebar here-->
             <?php get_sidebar(); ?>
